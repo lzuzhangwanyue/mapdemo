251d6dffaa35d133dbb6cff3f9e43275dc2b5ae6
lzugis <1004740957@qq.com>
2018-03-28 09:45:28 +0800

    init

--- js/treeData.js
+++ js/treeData.js
@@ -12,87 +12,87 @@ var treeData =[
 		                open:true,
 		                children:[{
 	                        name: "加载wms",
-	                        src: "example/openlayers/ol3/wms.html",
+	                        src: "example/openlayers/wms.html",
 		                    senior:false
 	                    },{
 	                        name: "加载OSM",
-	                        src: "example/openlayers/ol3/osm.html",
+	                        src: "example/openlayers/osm.html",
 		                    senior:false
 	                    },{
 	                        name: "加载必应地图",
-	                        src: "example/openlayers/ol3/public_map/bingmap.html",
+	                        src: "example/openlayers/public_map/bingmap.html",
 		                    senior:false
 	                    },{
 	                        name: "在线天地图",
-	                        src: "example/openlayers/ol3/public_map/tdtonline.html",
+	                        src: "example/openlayers/public_map/tdtonline.html",
 		                    senior:false
 	                    },{
 	                        name: "加载Arcgis切片",
-	                        src: "example/openlayers/ol3/agstile.html",
+	                        src: "example/openlayers/agstile.html",
 		                    senior:false
 	                    },{
 	                        name: "属性聚类",
-	                        src: "example/openlayers/ol3/cluster_attr.html",
+	                        src: "example/openlayers/cluster_attr.html",
 		                    senior:false
 	                    },{
 	                        name: "距离聚类",
-	                        src: "example/openlayers/ol3/cluster_pixel.html",
+	                        src: "example/openlayers/cluster_pixel.html",
 		                    senior:false
 	                    },{
 	                        name: "卷帘工具",
-	                        src: "example/openlayers/ol3/swipe.html",
+	                        src: "example/openlayers/swipe.html",
 		                    senior:false
 	                    },{
 	                        name: "地图控制",
-	                        src: "example/openlayers/ol3/control.html",
+	                        src: "example/openlayers/control.html",
 		                    senior:false
 	                    },{
 	                        name: "图形绘制",
-	                        src: "example/openlayers/ol3/draw.html",
+	                        src: "example/openlayers/draw.html",
 		                    senior:false
 	                    },{
 	                        name: "测量",
-	                        src: "example/openlayers/ol3/measure.html",
+	                        src: "example/openlayers/measure.html",
 		                    senior:false
 	                    },{
 	                        name: "修改编辑",
-	                        src: "example/openlayers/ol3/modify.html",
+	                        src: "example/openlayers/modify.html",
 		                    senior:false
 	                    },{
 	                        name: "多地图",
-	                        src: "example/openlayers/ol3/multimap.html",
+	                        src: "example/openlayers/multimap.html",
 		                    senior:false
 	                    },{
 	                        name: "信息框",
-	                        src: "example/openlayers/ol3/popup.html",
+	                        src: "example/openlayers/popup.html",
 		                    senior:false
 	                    },{
 	                        name: "选择对象",
-	                        src: "example/openlayers/ol3/select.html",
+	                        src: "example/openlayers/select.html",
 		                    senior:false
 	                    },{
 	                        name: "wkt操作",
-	                        src: "example/openlayers/ol3/wkt.html",
+	                        src: "example/openlayers/wkt.html",
 		                    senior:false
 	                    },{
 	                        name: "动画效果",
-	                        src: "example/openlayers/ol3/animation.html",
+	                        src: "example/openlayers/animation.html",
 		                    senior:false
 	                    },{
 	                        name: "地图资源设置",
-	                        src: "example/openlayers/ol3/lyr-source.html",
+	                        src: "example/openlayers/lyr-source.html",
 		                    senior:false
 	                    },{
 	                        name: "地图target设置",
-	                        src: "example/openlayers/ol3/target.html",
+	                        src: "example/openlayers/target.html",
 		                    senior:false
 	                    },{
 	                        name: "地图缩放",
-	                        src: "example/openlayers/ol3/zoom.html",
+	                        src: "example/openlayers/zoom.html",
 		                    senior:false
 	                    },{
 	                        name: "POI展示",
-	                        src: "example/openlayers/ol3/icon.html",
+	                        src: "example/openlayers/icon.html",
 		                    senior:false
 	                    }]
                 	},
@@ -101,51 +101,51 @@ var treeData =[
 		                open:false,
 		                children:[{
 	                        name: "动态效果展示",
-	                        src: "example/openlayers/ol3/dynamic-data.html",
+	                        src: "example/openlayers/dynamic-data.html",
 		                    senior:true
 	                    },{
 	                        name: "动态点展示",
-	                        src: "example/openlayers/ol3/feature-animation.html",
+	                        src: "example/openlayers/feature-animation.html",
 		                    senior:true
 	                    },{
 	                        name: "添加geojson",
-	                        src: "example/openlayers/ol3/geojson.html",
+	                        src: "example/openlayers/geojson.html",
 		                    senior:true
 	                    },{
 	                        name: "断点值渲染",
-	                        src: "example/openlayers/ol3/breakrender.html",
+	                        src: "example/openlayers/breakrender.html",
 		                    senior:true
 	                    },{
 	                        name: "图层控制1",
-	                        src: "example/openlayers/ol3/layerswitch1.html",
+	                        src: "example/openlayers/layerswitch1.html",
 		                    senior:true
 	                    },{
 	                        name: "图层控制2",
-	                        src: "example/openlayers/ol3/layerswitch2.html",
+	                        src: "example/openlayers/layerswitch2.html",
 		                    senior:true
 	                    },{
 	                        name: "绘制带箭头的线",
-	                        src: "example/openlayers/ol3/layerswitch2.html",
+	                        src: "example/openlayers/layerswitch2.html",
 		                    senior:true
 	                    },{
 	                        name: "动态点展示",
-	                        src: "example/openlayers/ol3/point-animation-overlay.html",
+	                        src: "example/openlayers/point-animation-overlay.html",
 		                    senior:true
 	                    },{
 	                        name: "统计图",
-	                        src: "example/openlayers/ol3/echart.html",
+	                        src: "example/openlayers/echart.html",
 		                    senior:true
 	                    },{
 	                        name: "仪表盘",
-	                        src: "example/openlayers/ol3/echart.html",
+	                        src: "example/openlayers/echart.html",
 		                    senior:true
 	                    },{
 	                        name: "矩形选择",
-	                        src: "example/openlayers/ol3/boxselect.html",
+	                        src: "example/openlayers/boxselect.html",
 		                    senior:true
 	                    },{
 	                        name: "地图放大镜",
-	                        src: "example/openlayers/ol3/layer-spy.html",
+	                        src: "example/openlayers/layer-spy.html",
 		                    senior:true
 	                    }]
                 	}
